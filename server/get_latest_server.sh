46f54a8ae551d05dec0b7adc28d18900a4953283
xiaowen <77304905+xiaowen88@users.noreply.github.com>
2025-08-06 01:59:48 +0800

    Update get_latest_server.sh

--- server/get_latest_server.sh
+++ server/get_latest_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LATEST_VER=$(curl -s https://api.github.com/repos/perfect-panel/ppanel/releases/latest | jq -r '.tag_name')
-SERVER_URL="https://github.com/perfect-panel/ppanel/releases/download/${LATEST_VER}/ppanel-server-${LATEST_VER}-linux-amd64.tar.gz"
+SERVER_URL="wget -O ppanel-server.tar.gz https://github.com/perfect-panel/ppanel/releases/download/v0.1.1/ppanel-server-v0.1.1-linux-amd64.tar.gz"
 mkdir -p /opt/ppanel
 wget -O /opt/ppanel-server.tar.gz $SERVER_URL
 tar -xvf /opt/ppanel-server.tar.gz -C /opt/ppanel/
