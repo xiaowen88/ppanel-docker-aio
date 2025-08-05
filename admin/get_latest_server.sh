0407369be7edfda8d73868317a409cb5821d47bd
xiaowen <77304905+xiaowen88@users.noreply.github.com>
2025-08-06 02:17:37 +0800

    Update get_latest_server.sh

--- admin/get_latest_server.sh
+++ admin/get_latest_server.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# 固定使用 v1.1.5 版本
-SERVER_URL="https://github.com/perfect-panel/ppanel-web/releases/download/v1.1.5/ppanel-admin-web.tar.gz"
-
-# 创建目录
+# 下载管理端
+log_info "下载管理端..."
 mkdir -p /opt/ppanel
-
-# 下载文件
-wget -O /opt/ppanel-admin-web.tar.gz "$SERVER_URL"
-
-# 解压
+wget -O /opt/ppanel-admin-web.tar.gz https://github.com/perfect-panel/ppanel-web/releases/download/v1.0.0-beta.6/ppanel-admin-web.tar.gz
 tar -xvf /opt/ppanel-admin-web.tar.gz -C /opt/ppanel/
