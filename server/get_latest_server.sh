ca5a706d767cf2d085a2d49d9847cbba1ac0629f
xiaowen <77304905+xiaowen88@users.noreply.github.com>
2025-08-06 02:34:11 +0800

    Update get_latest_server.sh

--- server/get_latest_server.sh
+++ server/get_latest_server.sh
@@ -2,5 +2,5 @@
 # 下载服务端
 log_info "下载服务端..."
 mkdir -p /opt/ppanel
-wget -O /opt/ppanel-server.tar.gz https://github.com/perfect-panel/ppanel/releases/download/v0.1.1/ppanel-server-v0.1.1-linux-amd64.tar.gz
+wget -O /opt/ppanel-server.tar.gz https://github.com/perfect-panel/ppanel/releases/download/v0.1.7-beta3/ppanel-server-v0.1.7-beta3-linux-amd64.tar.gz
 tar -xvf /opt/ppanel-server.tar.gz -C /opt/ppanel/
